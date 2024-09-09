477b3060d7571b08f31949f0a63945f0b9caf58a
Eve Zhang <xuyinz1@uci.edu>
2024-09-09 15:26:46 -0700

    add frame to project card

--- src/components/portfolio/PortfolioCard.js
+++ src/components/portfolio/PortfolioCard.js
@@ -5,28 +5,40 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import Style from "./PortfolioCard.module.scss"
+import { Box } from "@mui/material";
+import classNames from "classnames";
+
+const iconClass = "fa fa-circle";
 
 export default function PortfolioCard({project}) {
   const { image, live, source, title, intro } = project;
   let detectedDarkMode = JSON.parse(localStorage.getItem('darkMode'));
   return (
-	<Card className={detectedDarkMode? Style.dark : Style.light}>
-		<CardActionArea href={source}>
-			<CardMedia
-			component="img"
-			height="200"
-			image={image}
-			alt="project img"
-			/>
-			<CardContent>
-				<Typography gutterBottom variant="h5" component="div">
-					{title}
-				</Typography>
-				<Typography variant="body2">
-					{intro}
-				</Typography>
-			</CardContent>
-		</CardActionArea>
-	</Card>
+	<Box component={'section'} className={classNames(Style.terminal, Style.shadowed)} borderRadius={'0.5rem'} mb={'4rem'}>
+         <Box sx={{backgroundColor: '#8c8c8c'}} p={'0.5rem'} borderRadius={'0.5rem 0.5rem 0 0'}
+              fontSize={'1rem'}>
+            <i className={classNames(iconClass, Style.red)}/>
+            <i className={classNames(iconClass, Style.amber)}/>
+            <i className={classNames(iconClass, Style.green)}/>
+         </Box>
+		<Card className={detectedDarkMode? Style.dark : Style.light}>
+			<CardActionArea href={source}>
+				<CardMedia
+				component="img"
+				height="200"
+				image={image}
+				alt="project img"
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						{title}
+					</Typography>
+					<Typography variant="body1">
+						{intro}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	</Box>
   );
 }
